f982acd1523774dbbb075d7c8347cde93fbe2cd5
Skinn1Shady <beetlejucing22@gmail.com>
2025-01-09 22:12:05 +0100

    New css

--- poroto.js
+++ poroto.js
@@ -1,20 +1,14 @@
 function addRecommendation() {
-    // Get the message of the new recommendation
     let recommendation = document.getElementById("new_recommendation");
-    // If the user has left a recommendation, display a pop-up
     if (recommendation.value != null && recommendation.value.trim() != "") {
       console.log("New recommendation added");
-      //Call showPopup here
       showPopup(true);
   
-      // Create a new 'recommendation' element and set it's value to the user's message
       var element = document.createElement("div");
       element.setAttribute("class","recommendation");
       element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-      // Add this element to the end of the list of recommendations
       document.getElementById("all_recommendations").appendChild(element); 
       
-      // Reset the value of the textarea
       recommendation.value = "";
     }
   }
@@ -26,4 +20,15 @@ function addRecommendation() {
       document.getElementById('popup').style.visibility = 'hidden'
     }
   }
-  
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.topmenu');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
